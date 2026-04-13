dc36e5bc8e21e1b1fad885b954fecd6192421068
Derin Özcan <aliongithub@proton.me>
2026-04-13 08:52:04 +0300

    Add Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { registerPwa } from "./pwa.js";
+import { Analytics } from "@vercel/analytics/react";
 import "./styles.css";
 
 registerPwa();
@@ -9,5 +10,6 @@ registerPwa();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
